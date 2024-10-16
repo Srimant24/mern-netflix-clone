0deb60b194783c67b89a2ab7e37c397d7948114f
srimant0203 <srimant.acharjya@gmail.com>
2024-10-16 11:48:49 +0530

    commit

---
+++ backend/server.js
@@ -0,0 +1,51 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import path from "path";
+
+import authRoutes from "./routes/auth.route.js";
+import movieRoutes from "./routes/movie.route.js";
+import tvRoutes from "./routes/tv.route.js";
+import searchRoutes from "./routes/search.route.js";
+
+import { ENV_VARS } from "./config/envVars.js";
+import { connectDB } from "./config/db.js";
+import { protectRoute } from "./middleware/protectRoute.js";
+
+const app = express();
+
+
+const __dirname = path.resolve();
+
+
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+const corsOptions = {
+    origin:'http://localhost:5173/',
+    credentials:true
+}
+app.use(cors(corsOptions));
+
+const PORT = ENV_VARS.PORT;
+
+
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/movie", protectRoute, movieRoutes);
+app.use("/api/v1/tv", protectRoute, tvRoutes);
+app.use("/api/v1/search", protectRoute, searchRoutes);
+
+if (ENV_VARS.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
+
+app.listen(PORT, () => {
+	console.log("Server started at http://localhost:" + PORT);
+	connectDB();
+});
